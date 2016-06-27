354b6641b6ba550858a4784e7c5a7359f737d2e2
Brian Buller <brbuller@gmail.com>
2016-06-27 07:23:56 -0500

    Add comment explaining mainloop_windows.go

--- mainloop_windows.go
+++ mainloop_windows.go
@@ -1,5 +1,8 @@
 package main
 
+// Windows doesn't support process backgrounding like *nix.
+// So we have a separate loop for it.
+
 import "github.com/nsf/termbox-go"
 
 func mainLoop(memBolt *BoltDB, style Style) {
